3e912e4549b82302fb925a934bcd656bdaba42e8
Alex Moini <alexandermoini@gmail.com>
2022-10-03 10:11:22 -0600

    mean_pooling issues in progress

--- data_utils.py
+++ data_utils.py
@@ -1,20 +1,15 @@
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import pandas as pd
-from transformers import BertTokenizer
-
+from transformers import AutoTokenizer
+import numpy as np
 class PhraseDataset(Dataset):
     def __init__(self, path):
-        self.data = pd.read_csv(path)
-    def process(self, text):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        return tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+        self.data = pd.read_csv(path, header=0)
     def __getitem__(self, index):
         anchor = self.data.iloc[index]['anchor']
         target = self.data.iloc[index]['target']
-        label = self.data.iloc[index]['label']
-        anchor_input_ids, anchor_attention_masks = self.process(anchor)
-        target_input_ids, target_attention_masks = self.process(target)
-        return anchor_input_ids, anchor_attention_masks, target_input_ids, target_attention_masks, label
+        label = self.data.iloc[index]['score']
+        label = np.float32(label)
+        return anchor, target, label
     def __len__(self):
         return len(self.data)
-
--- transformer_class.py
+++ transformer_class.py
@@ -1,31 +1,51 @@
 import torch
 from transformers import AutoModel
+import torch.nn.functional as F
 
 class SimilarityModel(torch.nn.Module):
     def __init__(self, model_name):
         super(SimilarityModel, self).__init__()
-        self.bert = AutoModel.from_pretrained(model_name)
+        self.encoder = AutoModel.from_pretrained(model_name)
+
+    def mean_pooling(model_output, attention_mask):
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     def forward(self, anchor_input_ids, anchor_attention_mask, target_input_ids, target_attention_mask):
-        _anchor, anchor_pooled_output = self.bert(input_ids=anchor_input_ids, attention_mask=anchor_attention_mask)
-        _target, target_pooled_output = self.bert(input_ids=target_input_ids, attention_mask=target_attention_mask)
+        # encode input sequences
+        encoded_anchor = self.encoder(input_ids=anchor_input_ids, attention_mask=anchor_attention_mask)
+        encoded_target = self.encoder(input_ids=target_input_ids, attention_mask=target_attention_mask)
+        print(type(encoded_anchor))
+        print(type(anchor_attention_mask))
+
+        # mean pooling
+        anchor_pooled_output = self.mean_pooling(encoded_anchor.last_hidden_state, anchor_attention_mask)
+        target_pooled_output = self.mean_pooling(encoded_target.last_hidden_state, target_attention_mask)
+        # normalize
+        anchor_pooled_output = F.normalize(anchor_pooled_output, p=2, dim=1)
+        # cosine similarity
         return torch.cosine_similarity(anchor_pooled_output, target_pooled_output, dim=1)
     def predict(self, anchor_input_ids, anchor_attention_mask, target_input_ids, target_attention_mask):
         self.eval()
         with torch.no_grad():
             return self.forward(anchor_input_ids, anchor_attention_mask, target_input_ids, target_attention_mask)
 
-def train(model, train_dataloader, valid_dataloader, device, optimizer, loss_function, epochs):
+def train(model, tokenizer, train_dataloader, valid_dataloader, device, optimizer, loss_function, epochs):
     train_loss = []
     valid_loss = []
     for epoch in range(epochs):
         model.train()
-        for anchor_input_ids, anchor_attention_mask, target_input_ids, target_attention_mask, label in train_dataloader:
-            anchor_input_ids = anchor_input_ids.to(device)
-            anchor_attention_mask = anchor_attention_mask.to(device)
-            target_input_ids = target_input_ids.to(device)
-            target_attention_mask = target_attention_mask.to(device)
-            label = label.to(device)
+        for anchor_batch, target_batch, label_batch in train_dataloader:
+
+            anchor = tokenizer(list(anchor_batch), padding=True, truncation=True, return_tensors="pt")
+            target = tokenizer(list(target_batch), padding=True, truncation=True, return_tensors="pt")
+            anchor_input_ids = anchor['input_ids'].to(device)
+            anchor_attention_mask = anchor['attention_mask'].to(device)
+            target_input_ids = target['input_ids'].to(device)
+            target_attention_mask = target['attention_mask'].to(device)
+            label = torch.Tensor(label_batch).to(device)
+            model.to(device)
             optimizer.zero_grad()
             output = model(anchor_input_ids, anchor_attention_mask, target_input_ids, target_attention_mask)
             loss = loss_function(output, label)
